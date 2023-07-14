8d103d7c9608fc3d4bdeb31df633205e36a816ca
diivanovsky <diivanovsky@gmail.com>
2023-07-14 23:45:47 +0300

    1 task

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ def imt_count():
         height = int(input("Enter your height in centimetres: "))
         if weight and height < 1:
             raise TypeError
-        imt = (weight / ((height / 100) ** 2))
-        if imt < 17:
-            print("Bad news, you need to eat a lot")
+        imt = round((weight / ((height / 100) ** 2)), 1)
+        if imt < 16.5:
+            print(f"Your index is: {imt}. It's huge underweight, so close to death")
         elif 16 < imt < 18.6:
-            print("Not so good, you need additional breakfast, lunch and double dinner")
+            print(f"Your index is: {imt}.It's underweight")
         elif 18.5< imt < 26:
-            print("Keep on that's way")
+            print(f"Your index is: {imt}. It's normal weight")
         elif 24 < imt < 31:
-            print("Just a little bit and you'll become fat ass")
-        elif
+            print(f"Your index is: {imt}. It's overweight")
+        elif 29 < imt < 35:
+            print(f"Your index is: {imt}. It's obesity (First class)")
+        elif 34 < imt < 40:
+            print(f"Your index is: {imt}. It's obesity (Second class - severe)")
+        elif 40 < imt:
+            print(f"Your index is: {imt}. It's obesity (Second class - very severe)")
     except ValueError:
         print("Enter only numbers, without coma")
         return imt_count()
